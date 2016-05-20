612b78412cb9c6f335c2161a126a5ebf44ff1e4a
Yeis <jcrockyeis@gmail.com>
2016-05-20 13:35:52 -0700

    Cambios Business Logic , Stats Controller

--- PokedexFinalProject/PokedexFinalProject/BusinessLogic.cs
+++ PokedexFinalProject/PokedexFinalProject/BusinessLogic.cs
@@ -7,41 +7,55 @@ namespace PokedexFinalProject
 {
     public class BusinessLogic
     {
+        int Starttime;
+        int Endtime;
+        PokedexEntities context;
         public BusinessLogic()
         {
-
+            context = new PokedexEntities();
         }
 
         public LocalUser Login(string username, string password)
-        {
-            using (var context = new PokedexEntities())
-            {
-                int startime = DateTime.Now.Millisecond;
+        {  
+                Starttime = DateTime.Now.Millisecond;
                 IQueryable<Usuario> usuario = context.Usuarios.Where(u => u.Username == username && u.Password == password);
-                int endtime = DateTime.Now.Millisecond;
+                Endtime = DateTime.Now.Millisecond;
                 List<Usuario> user = new List<Usuario>(usuario);
 
                 //checamos si las credenciales fueron correctas 
                 if (user.Count > 0)
                 {
-                    LogData log = new LogData()
-                    {
-                        nombre = user[0].Username,
-                        tipo = "Login",
-                        fecha = DateTime.Now,
-                        UserId = user[0].UserID,
-                        exec_time = (endtime - startime)
-                    };
-                    
-                    context.LogDatas.Add(log);
-                    context.SaveChanges();
+                    AddLog(new LogData() {  nombre = user[0].Username, tipo = "Login", fecha = DateTime.Now, UserId = user[0].UserID , exec_time = (Endtime - Starttime)});
                     return new LocalUser(user[0].UserID, user[0].FirstName, user[0].LastName, user[0].DOB, user[0].Username, user[0].Password, user[0].Email, user[0].Admin, false);
                 }
                 else
                 {
                     return null;
                 }
-            }
+            
+        }
+        public List<ActiveUsers_Month_Result> Acitve_Users_Month()
+        {
+                Starttime = DateTime.Now.Millisecond;
+                List<ActiveUsers_Month_Result> result = context.ActiveUsers_Month().ToList();
+                Endtime = DateTime.Now.Millisecond;
+               
+                AddLog(new LogData() {   id= 10, nombre = "ActiveUsers_Month", tipo = "SP", fecha = DateTime.Now, UserId = SharedInstance.AppUser.UserID, exec_time = (Endtime - Starttime) });
+                return result;
+        }
+        public List<ActiveUsers_Week_Result> Acitve_Users_Week()
+        {
+                Starttime = DateTime.Now.Millisecond;
+                List<ActiveUsers_Week_Result> result = context.ActiveUsers_Week().ToList();
+                Endtime = DateTime.Now.Millisecond;
+
+                AddLog(new LogData() { id = 10, nombre = "ActiveUsers_Week", tipo = "SP", fecha = DateTime.Now, UserId = SharedInstance.AppUser.UserID, exec_time = (Endtime - Starttime) });
+                return result;
+        }
+        public void AddLog(LogData log )
+        {
+               // context.LogDatas.Add(log);
+                //context.SaveChanges();
         }
     }
 }
---
+++ PokedexFinalProject/PokedexFinalProject/Models/StatsViewModel.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace PokedexFinalProject.Models
+{
+    public class StatsViewModel
+    {
+        [DisplayName("Select Option")]
+        public int Selectedid { get; set; }
+        public IEnumerable<SelectListItem> Options { get; set; }
+        public string partialName { get; set; }
+
+
+
+
+        //Stats Collection
+        public IEnumerable<ActiveUsers_Month_Result> ActiveUsersMonth { get; set; }
+        public IEnumerable<ActiveUsers_Week_Result> ActiveUsersWeek { get; set; }
+
+    }
+}
